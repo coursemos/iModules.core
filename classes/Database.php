6e4c6939d9b03975f2cf78128cc5efed9a6d7f59
arzzcom <arzz@arzz.com>
2022-02-08 19:26:59 +0900

    Create Database.php

---
+++ classes/Database.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * 이 파일은 아이모듈의 일부입니다. (https://www.imodules.io)
+ *
+ * 데이터베이스 클래스를 정의한다.
+ *
+ * @file /classes/Database.php
+ * @author Arzz <arzz@arzz.com>
+ * @license MIT License
+ * @modified 2022. 1. 19.
+ */
+class Database {
+	/**
+	 * 데이터베이스 커넥션 정보
+	 */
+	private array $_connections = [];
+	
+	/**
+	 * 데이터베이스 인터페이스 정보
+	 */
+	private array $_interfaces = [];
+	
+	/**
+	 * 싱글톤 방식으로 데이터베이스클래스를 선언한다.
+	 */
+	private static Database $_instance;
+	public static function &getInstance():Database {
+		if (empty(self::$_instance) == true) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+	
+	/**
+	 * 데이터베이스 인터페이스 클래스를 가져온다.
+	 *
+	 * @param string $name 커넥션명
+	 * @param object $connector 데이터베이스정보
+	 * @return DatabaseInterface $interface
+	 */
+	public function getInterface(string $name,object $connector):DatabaseInterface {
+		if (isset($this->_interfaces[$name]) == true) return $this->_interfaces[$name];
+		
+		/**
+		 * 데이터베이스 정보를 이용하여 데이터베이스 서버 고유값을 구한다.
+		 */
+		$connection = sha1($connector->type.$connector->host.$connector->database);
+		$interface = null;
+		
+		if ($connector->type == 'mysql') {
+			$interface = new \Databases\mysql($this);
+		}
+		
+		if ($interface === null) {
+			$this->_parent->printError('NOT_FOUND_DATABASE_INTERFACE',null,$connector);
+		}
+		
+		/**
+		 * 이미 데이터베이스 커넥션정보가 있다면 해당 커넥션을 이용하고, 그렇지 않은 경우 커넥션을 생성한다.
+		 */
+		if (isset($this->_connections[$connection]) == true) {
+			$interface->setConnection($this->_connections[$connection]);
+		} else {
+			$this->_connections[$connection] = $interface->connect($connector);
+		}
+		
+		$this->_interfaces[$name] = $interface;
+		
+		return $this->_interfaces[$name];
+	}
+	
+	/**
+	 * 에러메시지를 출력한다.
+	 *
+	 * @param string $message 에러메시지
+	 * @param ?object $details 에러상세데이터
+	 */
+	public function printError(string $message,?object $details=null):void {
+		ErrorHandler::view('DATABASE_ERROR',$message,$details);
+	}
+}
+?>
