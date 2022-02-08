8d84dfd2116806c31127133666476fa4d4e9a136
arzzcom <arzz@arzz.com>
2022-02-08 19:22:34 +0900

    Create iModules.php

---
+++ classes/iModules.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 이 파일은 아이모듈의 일부입니다. (https://www.imodules.io)
+ *
+ * 아이모듈 코어 클래스로 모든 사이트 레이아웃 및 모듈, 위젯, 플러그인 클래스는 아이모듈 코어 클래스를 통해 호출된다.
+ * 이 클래스는 index.php 파일에 의해 선언되며 아이모듈과 관련된 모든 PHP파일에서 $IM 변수로 접근할 수 있다.
+ *
+ * @file /classes/iModules.php
+ * @author Arzz
+ * @license MIT License
+ * @version 4.0.0
+ * @modified 2022. 2. 8.
+ */
+class iModules {
+}
+?>
