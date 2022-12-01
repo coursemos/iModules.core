645bbd6f3af732e8010d61e5c53ff2bd787068d0
arzzcom <arzz@arzz.com>
2022-12-01 10:53:59 +0900

    파일 작성 규칙 변경

--- index.php
+++ index.php
@@ -3,19 +3,15 @@
  * 이 파일은 아이모듈의 일부입니다. (https://www.imodules.io)
  *
  * 사이트 최초 접속시 실행되는 파일로 기본설정을 불러오고 아이모듈 코어 클래스를 선언하여 요청된 페이지를 반환한다.
- * 
+ *
  * @file /index.php
  * @author Arzz <arzz@arzz.com>
  * @license MIT License
- * @modified 2022. 2. 15.
+ * @modified 2022. 12. 1.
  */
 require_once './configs/init.php';
 
 /**
- * 아이모듈코어를 선언하고, 레이아웃을 불러온다.
- * 
- * @see /classes/iModules.class.php
+ * 요청에 따라 응답한다.
  */
-$IM = iModules::getInstance();
-$IM->route();
-?>
+iModules::respond();
