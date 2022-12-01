5e589a83f2090e5dc4b87792d74c1c9e558dd239
arzzcom <arzz@arzz.com>
2022-12-01 10:52:30 +0900

    파일 작성 규칙 변경

--- configs/init.php
+++ configs/init.php
@@ -7,50 +7,50 @@
  * @file /configs/init.php
  * @author Arzz <arzz@arzz.com>
  * @license MIT License
- * @modified 2022. 2. 15.
+ * @modified 2022. 12. 1.
  */
 if (is_file('./configs/configs.php') == false) {
-	header("location: ./install");
-	exit;
+    header('location: ./install');
+    exit();
 }
 
 error_reporting(E_ALL);
-ini_set('display_errors',true);
+ini_set('display_errors', true);
 
 /**
  * 환경설정파일을 불러온다.
  */
 $_CONFIGS = new stdClass();
-$_CONFIGS->debug = false;
 $_CONFIGS->db = new stdClass();
-require_once __DIR__.'/configs.php';
+require_once realpath(__DIR__ . '/configs.php');
 
 /**
- * 클래스파일을 자동으로 불러오기위한 오토로더클래스를 불러온다.
+ * 환경설정을 불러오기위한 클래스를 초기화한다.
  */
-require_once __DIR__.'/../classes/Config.php';
-Config::init($_CONFIGS);
+require_once realpath(__DIR__ . '/../classes/Configs.php');
+Configs::init($_CONFIGS);
 
 /**
  * 아이모듈 상수정의
  * 아이모듈에서 사용되는 상수는 상수명 앞뒤를 언더바 2개로 감싼 형태로 정의힌다. (__[상수명]__)
- * 
+ *
  * __IM__ : 아이모듈 코어에 의해 PHP 파일이 실행되었는지 여부를 확인하는 상수
  * __IM_VERSION__ : 아이모듈 버전을 정의하는 상수. 빌드날짜는 포함되지 않음
  * __IM_DB_PREFIX__ : 아이모듈에서 생성되는 모든 DB 테이블앞에 붙는 PREFIX를 정의하는 상수
  * __IM_PATH__ : 아이모듈이 설치되어 있는 서버상의 경로 (./configs/configs.php 파일에 의해 정의)
  * __IM_DIR__ : 아이모듈이 설치되어 있는 웹브라우저상의 경로 (./configs/configs.php 파일에 의해 정의)
  */
-define('__IM__',true);
-define('__IM_VERSION__','4.0.0');
-define('__DEBUG_MODE__',$_CONFIGS->debug);
+define('__IM__', true);
+define('__IM_VERSION__', '4.0.0');
 
 /**
  * 클래스파일을 자동으로 불러오기위한 오토로더클래스를 불러온다.
  */
-require_once Config::path().'/classes/AutoLoader.php';
-AutoLoader::register('/classes');
-AutoLoader::register('/vendor','/src');
+require_once Configs::path() . '/classes/AutoLoader.php';
+AutoLoader::init();
+AutoLoader::register('/', '/');
+AutoLoader::register('/', '/classes');
+AutoLoader::register('/vendor', '/src');
 
 /**
  * 에러클래스를 초기화한다.
@@ -63,9 +63,8 @@ ErrorHandler::init();
  */
 header('X-UA-Compatible: IE=Edge');
 header('X-XSS-Protection: 1');
-header('Content-type: text/html; charset=utf-8');
 header('Expires: 0');
-header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Cache-Control: pre-check=0, post-check=0, max-age=0');
 header('Pragma: no-cache');
