7a55668adb9f440a1ad935c1dfe903d4aefd3804
arzzcom <arzz@arzz.com>
2022-12-01 11:39:59 +0900

    모듈 기본 클래스

--- scripts/Module.ts
+++ scripts/Module.ts
@@ -9,22 +9,23 @@
  * @modified 2022. 12. 1.
  */
 class Module {
-    dom;
+    dom?: Dom;
+
     /**
      * 모듈 클래스를 생성한다.
      *
      * @param {Dom} dom - 모듈 컨텍스트의 Dom 객체
      */
-    constructor(dom) {
+    constructor(dom?: Dom) {
         if (dom instanceof Dom === true) {
             this.dom = dom;
-        }
-        else {
+        } else {
             this.dom = null;
         }
     }
+
     /**
      * 해당 모듈의 Dom 객체를 초기화한다.
      */
-    init() { }
+    init(): void {}
 }
