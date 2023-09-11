e373d74c8fe34679508ccc30076d81ae6ecc2f95
arzzcom <arzz@arzz.com>
2023-09-11 11:33:41 +0900

    매시간, 매일 작업 구분

--- classes/Cron.php
+++ classes/Cron.php
@@ -13,10 +13,8 @@ class Cron
 {
     /**
      * 자동화 작업 클래스를 초기화한다.
-     *
-     * @param array $hosts - 자동화작업을 수행할 도메인 호스트명
      */
-    public function __construct(array $hosts = [])
+    public function __construct()
     {
         error_reporting(E_ALL);
         ini_set('display_errors', true);
@@ -37,7 +35,76 @@ class Cron
          */
         foreach (Modules::all() as $module) {
             if ($module->hasPackageProperty('CRON') == true) {
-                $this->_daily($module);
+                $this->_execute($module);
+            }
+        }
+
+        exit();
+    }
+
+    /**
+     * 자동화 작업을 수행한다.
+     *
+     * @param Component $component 자동화작업을 수행할 컴포넌트
+     */
+    private function _execute(Component &$component): void
+    {
+        $hour = date('G');
+        $me = $component;
+
+        $insert = [];
+        $insert['date'] = date('Y-m-d');
+        $insert['hour'] = $hour;
+        $insert['component_type'] = $me->getType();
+        $insert['component_name'] = $me->getName();
+
+        // @todo 데일리 작업 동작시간 환경설정
+        if (isset($_SERVER['SSH_CLIENT']) === true || $hour == 4) {
+            if (is_file($component->getPath() . '/crons/daily.php') == true) {
+                if (isset($_SERVER['SSH_CLIENT']) === true) {
+                    include $component->getPath() . '/crons/daily.php';
+                } else {
+                    $start = Format::microtime(3) * 1000;
+                    ob_start();
+                    $executed = include $component->getPath() . '/crons/daily.php';
+                    $logs = ob_get_clean();
+
+                    if ($executed === true) {
+                        $runtime = Format::microtime(3) * 1000 - $start;
+                        $insert['type'] = 'DAILY';
+                        $insert['started_at'] = $start;
+                        $insert['runtime'] = $runtime;
+                        $insert['logs'] = strlen($logs) > 0 ? $logs : null;
+
+                        iModules::db('default')
+                            ->replace(iModules::table('crons'), $insert)
+                            ->execute();
+                    }
+                }
+            }
+        }
+
+        if (is_file($component->getPath() . '/crons/hourly.php') == true) {
+            if (isset($_SERVER['SSH_CLIENT']) === true) {
+                include $component->getPath() . '/crons/hourly.php';
+            } else {
+                $start = Format::microtime(3) * 1000;
+                ob_start();
+                $executed = include $component->getPath() . '/crons/hourly.php';
+                print_r($_SERVER);
+                $logs = ob_get_clean();
+
+                if ($executed === true) {
+                    $runtime = Format::microtime(3) * 1000 - $start;
+                    $insert['type'] = 'DAILY';
+                    $insert['started_at'] = $start;
+                    $insert['runtime'] = $runtime;
+                    $insert['logs'] = strlen($logs) > 0 ? $logs : null;
+
+                    iModules::db('default')
+                        ->replace(iModules::table('crons'), $insert)
+                        ->execute();
+                }
             }
         }
     }
@@ -55,6 +122,19 @@ class Cron
 
             ob_start();
             include $component->getPath() . '/crons/daily.php';
+            $logs = ob_get_clean();
+
+            // @todo 데이터베이스에 기록
+            echo $logs;
+
+            echo PHP_EOL;
+        }
+
+        if (is_file($component->getPath() . '/crons/hourly.php') == true) {
+            $me = $component;
+
+            ob_start();
+            include $component->getPath() . '/crons/hourly.php';
             $logs = ob_get_clean();
 
             // @todo 데이터베이스에 기록
