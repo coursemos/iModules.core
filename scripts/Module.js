106a6ff79f54dd3c5a66728ceef20143703f7982
arzzcom <arzz@arzz.com>
2024-01-26 06:00:58 +0900

    컴포넌트 클래스 상속

--- scripts/Module.js
+++ scripts/Module.js
@@ -8,15 +8,16 @@
  * @license MIT License
  * @modified 2023. 6. 10.
  */
-class Module {
-    name;
+class Module extends Component {
     /**
      * 모듈 클래스를 생성한다.
      *
      * @param {string} name - 모듈명
      */
     constructor(name) {
+        super();
         this.name = name;
+        this.type = 'module';
     }
     /**
      * 해당 모듈의 Dom 객체를 초기화한다.
@@ -24,35 +25,4 @@ class Module {
      * @param {Dom} $dom - 현재 모듈의 DOM 객체
      */
     init($dom) { }
-    /**
-     * 프로세스 URL 경로를 가져온다.
-     *
-     * @param {string} path - 실행경로
-     * @return {string} processUrl
-     */
-    getProcessUrl(path) {
-        return iModules.getProcessUrl('module', this.name, path);
-    }
-    /**
-     * 언어팩을 불러온다.
-     *
-     * @param string $text 언어팩코드
-     * @param ?array $placeHolder 치환자
-     * @return array|string|null $message 치환된 메시지
-     */
-    async getText(text, placeHolder = null) {
-        const paths = ['/modules/' + this.name, '/'];
-        return Language.getText(text, placeHolder, paths);
-    }
-    /**
-     * 언어팩 문자열이 위치할 DOM 을 반환하고, 언어팩이 비동기적으로 로딩되면 언어팩 내용으로 변환한다.
-     *
-     * @param string $text 언어팩코드
-     * @param ?array $placeHolder 치환자
-     * @return array|string|null $message 치환된 메시지
-     */
-    printText(text, placeHolder = null) {
-        const paths = ['/modules/' + this.name, '/'];
-        return Language.printText(text, placeHolder, paths);
-    }
 }
--- scripts/Module.ts
+++ scripts/Module.ts
@@ -6,18 +6,19 @@
  * @file /scripts/Module.ts
  * @author Arzz <arzz@arzz.com>
  * @license MIT License
- * @modified 2023. 6. 10.
+ * @modified 2024. 1. 26.
  */
-class Module {
-    name: string;
-
+class Module extends Component {
     /**
      * 모듈 클래스를 생성한다.
      *
      * @param {string} name - 모듈명
      */
     constructor(name: string) {
+        super();
+
         this.name = name;
+        this.type = 'module';
     }
 
     /**
@@ -26,38 +27,4 @@ class Module {
      * @param {Dom} $dom - 현재 모듈의 DOM 객체
      */
     init($dom: Dom): void {}
-
-    /**
-     * 프로세스 URL 경로를 가져온다.
-     *
-     * @param {string} path - 실행경로
-     * @return {string} processUrl
-     */
-    getProcessUrl(path: string): string {
-        return iModules.getProcessUrl('module', this.name, path);
-    }
-
-    /**
-     * 언어팩을 불러온다.
-     *
-     * @param string $text 언어팩코드
-     * @param ?array $placeHolder 치환자
-     * @return array|string|null $message 치환된 메시지
-     */
-    async getText(text: string, placeHolder: { [key: string]: string } = null): Promise<string | object> {
-        const paths: string[] = ['/modules/' + this.name, '/'];
-        return Language.getText(text, placeHolder, paths);
-    }
-
-    /**
-     * 언어팩 문자열이 위치할 DOM 을 반환하고, 언어팩이 비동기적으로 로딩되면 언어팩 내용으로 변환한다.
-     *
-     * @param string $text 언어팩코드
-     * @param ?array $placeHolder 치환자
-     * @return array|string|null $message 치환된 메시지
-     */
-    printText(text: string, placeHolder: { [key: string]: string } = null): string {
-        const paths: string[] = ['/modules/' + this.name, '/'];
-        return Language.printText(text, placeHolder, paths);
-    }
 }
