9331894dcc85a702b677f3c25d1b60cb4d7224f4
arzzcom <arzz@arzz.com>
2024-03-04 04:30:50 +0900

    자동화작업 메시지 출력관련 함수 추가

--- classes/Cron.php
+++ classes/Cron.php
@@ -7,10 +7,25 @@
  * @file /classes/Cron.php
  * @author Arzz <arzz@arzz.com>
  * @license MIT License
- * @modified 2023. 8. 21.
+ * @modified 2024. 3. 4.
  */
 class Cron
 {
+    /**
+     * @var bool $is_ssh SSH 클라이언트를 통해 실행중인지 여부
+     */
+    private static bool $is_ssh;
+
+    /**
+     * @var bool $linebreak 자동화실행 중 출력한 메시지에서 줄바꿈을 출력하였는지 여부
+     */
+    private static bool $linebreak = false;
+
+    /**
+     * @var int $progress 자동화실행 중 진행률을 출력한 횟수
+     */
+    private static int $progress = 0;
+
     /**
      * 자동화 작업 클래스를 초기화한다.
      */
@@ -19,6 +34,8 @@ class Cron
         error_reporting(E_ALL);
         ini_set('display_errors', true);
         Modules::init();
+
+        self::$is_ssh = isset($_SERVER['SSH_CLIENT']) == true;
     }
 
     /**
@@ -58,36 +75,8 @@ class Cron
         $insert['component_type'] = $me->getType();
         $insert['component_name'] = $me->getName();
 
-        $is_ssh = isset($_SERVER['SSH_CLIENT']) == true;
-
-        // @todo 데일리 작업 동작시간 환경설정
-        if ($is_ssh === true || $hour == 4) {
-            if (is_file($component->getPath() . '/crons/daily.php') == true) {
-                if (isset($_SERVER['SSH_CLIENT']) === true) {
-                    include $component->getPath() . '/crons/daily.php';
-                } else {
-                    $start = Format::microtime(3) * 1000;
-                    ob_start();
-                    $executed = include $component->getPath() . '/crons/daily.php';
-                    $logs = ob_get_clean();
-
-                    if ($executed === true) {
-                        $runtime = Format::microtime(3) * 1000 - $start;
-                        $insert['type'] = 'DAILY';
-                        $insert['started_at'] = $start;
-                        $insert['runtime'] = $runtime;
-                        $insert['logs'] = strlen($logs) > 0 ? $logs : null;
-
-                        iModules::db('default')
-                            ->replace(iModules::table('crons'), $insert)
-                            ->execute();
-                    }
-                }
-            }
-        }
-
         if (is_file($component->getPath() . '/crons/hourly.php') == true) {
-            if ($is_ssh === true) {
+            if (self::$is_ssh === true) {
                 include $component->getPath() . '/crons/hourly.php';
             } else {
                 $start = Format::microtime(3) * 1000;
@@ -109,41 +98,94 @@ class Cron
                 }
             }
         }
+
+        // @todo 데일리 작업 동작시간 환경설정
+        if (self::$is_ssh === true || $hour == 4) {
+            if (is_file($component->getPath() . '/crons/daily.php') == true) {
+                if (self::$is_ssh === true) {
+                    include $component->getPath() . '/crons/daily.php';
+                } else {
+                    $start = Format::microtime(3) * 1000;
+                    ob_start();
+                    $executed = include $component->getPath() . '/crons/daily.php';
+                    $logs = ob_get_clean();
+
+                    if ($executed === true) {
+                        $runtime = Format::microtime(3) * 1000 - $start;
+                        $insert['type'] = 'DAILY';
+                        $insert['started_at'] = $start;
+                        $insert['runtime'] = $runtime;
+                        $insert['logs'] = strlen($logs) > 0 ? $logs : null;
+
+                        iModules::db('default')
+                            ->replace(iModules::table('crons'), $insert)
+                            ->execute();
+                    }
+                }
+            }
+        }
     }
 
     /**
-     * 매일 수행하는 자동화 작업을 수행한다.
+     * 자동화작업인 경우에만 디버그메시지를 출력한다.
      *
-     * @param Component $component 자동화작업을 수행할 컴포넌트
-     * @return void  description
+     * @param ...mixed $message 출력할 메시지
      */
-    private function _daily(Component &$component): void
+    public static function message(mixed ...$messages): void
     {
-        if (is_file($component->getPath() . '/crons/daily.php') == true) {
-            $me = $component;
+        self::$progress = 0;
 
-            ob_start();
-            include $component->getPath() . '/crons/daily.php';
-            $logs = ob_get_clean();
+        if (defined('__IM_CRON__') == true) {
+            if (self::$linebreak == false) {
+                echo PHP_EOL;
+            }
 
-            // @todo 데이터베이스에 기록
-            echo $logs;
+            $blank = true;
+            foreach ($messages as $message) {
+                if ($blank == false) {
+                    echo ' ';
+                    $blank = true;
+                }
+                if (is_string($message) == true || is_numeric($message) == true) {
+                    echo $message;
+                    $blank = false;
+                } else {
+                    echo PHP_EOL;
+                    var_dump($message);
+                    $blank = true;
+                }
+            }
 
+            echo PHP_EOL;
+            self::$linebreak = true;
+
+            if (self::$is_ssh == true) {
+                flush();
+            }
+        }
+    }
+
+    /**
+     * 자동화작업 진행률을 표시하기 위해 숫자, 또는 지정된 문자열을 출력한다.
+     * 100개가 출력된 뒤 줄바꿈을 출력한다.
+     *
+     * @param string $char 출력할 문자열 (NULL 인 경우 진행률 순서를 0-9 로 표시한다.)
+     */
+    public static function progress(string $char = null): void
+    {
+        if (self::$progress == 0 && self::$linebreak == false) {
             echo PHP_EOL;
         }
 
-        if (is_file($component->getPath() . '/crons/hourly.php') == true) {
-            $me = $component;
-
-            ob_start();
-            include $component->getPath() . '/crons/hourly.php';
-            $logs = ob_get_clean();
-
-            // @todo 데이터베이스에 기록
-            echo $logs;
-
+        echo $char ?? self::$progress % 10;
+        if (self::$progress % 100 == 99) {
             echo PHP_EOL;
         }
-        exit();
+        self::$progress++;
+        self::$linebreak = false;
+
+        if (self::$is_ssh == true) {
+            flush();
+        }
     }
 }
