8e54d9374d5fe0e754e75f15bef146f4ec345c05
arzzcom <arzz@arzz.com>
2024-03-21 09:49:59 +0900

    Create index.php

---
+++ themes/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 이 파일은 아이모듈의 일부입니다. (https://www.imodules.io)
+ *
+ * 직접접근을 허용하지 않는 디렉토리의 경우, 메인페이지로 이동한다.
+ *
+ * @file /themes/index.php
+ * @author Arzz <arzz@arzz.com>
+ * @license MIT License
+ * @modified 2024. 3. 8.
+ */
+header('location: ../');
+exit();
