5becf2678562090ab472a2b43563872a0ee1bccb
arzzcom <arzz@arzz.com>
2024-08-08 18:06:52 +0900

    rewrite 오류 수정

--- classes/iModules.php
+++ classes/iModules.php
@@ -422,7 +422,7 @@ class iModules
         /**
          * 요청된 주소와 경로가 다를 경우, 정상적인 경로로 리다이렉트한다.
          */
-        if (Request::url($route->getDomain()->isRewrite() == true ? false : ['route']) != $route->getUrl(true)) {
+        if (Request::url(false) != $route->getUrl(true)) {
             Header::location(Request::combine($route->getUrl(true), Request::query()));
             exit();
         }
@@ -518,15 +518,7 @@ class iModules
         /**
          * 요청된 주소와 경로가 다를 경우, 정상적인 경로로 리다이렉트한다.
          */
-        if (Request::url($route->getDomain()->isRewrite() == true ? false : ['route']) != $route->getUrl(true)) {
-            Header::location(Request::combine($route->getUrl(true), Request::query()));
-            exit();
-        }
-
-        /**
-         * 요청된 주소와 경로가 다를 경우, 정상적인 경로로 리다이렉트한다.
-         */
-        if (Request::url($route->getDomain()->isRewrite() == true ? false : ['route']) != $route->getUrl(true)) {
+        if (Request::url(false) != $route->getUrl(true)) {
             Header::location(Request::combine($route->getUrl(true), Request::query()));
             exit();
         }
